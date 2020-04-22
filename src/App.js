bb9a9bd738c6c1f77aa880f5f125be4cb426e3cb
Ben Whitney <ben.whitn@gmail.com>
2020-04-22 11:34:06 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,157 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "reactstrap";
+import NavBar from "./NavBar";
+import ErrorMessage from "./ErrorMessage";
+import Welcome from "./Welcome";
+import "bootstrap/dist/css/bootstrap.css";
+import config from "./Config";
+import { UserAgentApplication } from "msal";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.userAgentApplication = new UserAgentApplication({
+      auth: {
+        clientId: config.appId,
+        authority: config.authority,
+        validateAuthority: false,
+      },
+      cache: {
+        cacheLocation: "localStorage",
+        storeAuthStateInCookie: true,
+      },
+    });
+
+    var user = this.userAgentApplication.getAccount();
+
+    this.state = {
+      isAuthenticated: user !== null,
+      user: {},
+      error: null,
+    };
+
+    console.log(user);
+
+    if (user) {
+      // Enhance user object with data from Graph
+      this.getUserProfile();
+    }
+  }
+
+  async login() {
+    try {
+      await this.userAgentApplication.loginPopup({
+        scopes: config.scopes,
+        prompt: "select_account",
+      });
+
+      await this.getUserProfile();
+    } catch (err) {
+      var error = {};
+
+      console.log(err);
+
+      error = {
+        message: err.message,
+        debug: JSON.stringify(err),
+      };
+
+      this.setState({
+        isAuthenticated: false,
+        user: {},
+        error: error,
+      });
+    }
+  }
+
+  logout() {
+    this.userAgentApplication.logout();
+  }
+
+  async getUserProfile() {
+    try {
+      // Get the access token silently
+      // If the cache contains a non-expired token, this function
+      // will just return the cached token. Otherwise, it will
+      // make a request to the Azure OAuth endpoint to get a token
+
+      var accessToken = await this.userAgentApplication.acquireTokenSilent({
+        scopes: config.scopes,
+      });
+
+      if (accessToken) {
+        // TEMPORARY: Display the token in the error flash
+        this.setState({
+          isAuthenticated: true,
+          error: { message: "Access token:", debug: accessToken.accessToken },
+        });
+      }
+    } catch (err) {
+      var error = {};
+
+      console.log(err);
+      error = {
+        message: err.message,
+        debug: JSON.stringify(err),
+      };
+      this.setState({
+        isAuthenticated: false,
+        user: {},
+        error: error,
+      });
+    }
+  }
+
+  render() {
+    let error = null;
+    if (this.state.error) {
+      error = (
+        <ErrorMessage
+          message={this.state.error.message}
+          debug={this.state.error.debug}
+        />
+      );
+    }
+
+    return (
+      <Router>
+        <div>
+          <NavBar
+            isAuthenticated={this.state.isAuthenticated}
+            authButtonMethod={
+              this.state.isAuthenticated
+                ? this.logout.bind(this)
+                : this.login.bind(this)
+            }
+            user={this.state.user}
+          />
+          <Container>
+            {error}
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <Welcome
+                  {...props}
+                  isAuthenticated={this.state.isAuthenticated}
+                  user={this.state.user}
+                  authButtonMethod={this.login.bind(this)}
+                />
+              )}
+            />
+          </Container>
+        </div>
+      </Router>
+    );
+  }
+
+  setErrorMessage(message, debug) {
+    this.setState({
+      error: { message: message, debug: debug },
+    });
+  }
+}
+
+export default App;
---
+++ src/Config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  appId: "appID",
+  redirectUri: "http://localhost:3000",
+  authority:
+    "https://tenantID.b2clogin.com/tenantID.onmicrosoft.com/B2C_1_SUSI",
+  validateAuthority: false,
+  scopes: ["https://graph.microsoft.com/User.Read"],
+};
